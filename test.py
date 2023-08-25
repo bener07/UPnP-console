f29d56aa7c752aa079332662b0ecbd6bf3b662e9
bener07 <bernandre07@gmail.com>
2023-08-25 13:13:47 +0100

    just to be safe

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
+#!/bin/python3
 import socket
 import sys
-dst = "192.168.0.2"
+dst = "192.168.0.0"
 if len(sys.argv) > 1:
     dst = sys.argv[1]
 st = "upnp:rootdevice"
@@ -8,13 +9,13 @@ if len(sys.argv) > 2:
     st = sys.argv[2]
 msg = [
     'NOTIFY * HTTP/1.1',
-    'Host:239.255.255.250:1900',
+    'Host: 239.255.255.255:1900',
     'NT:'+st,
     'Man:"ssdp:discover"',
     'MX:20',
     '']
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-s.sendto('\r\n'.join(msg).encode()+b'\r\n', (dst, 1900) )
+s.sendto('\r\n'.join(msg).encode()+b'\r\n', (dst, 37443) )
 try:
     data, addr = s.recvfrom(32*1024)
 except socket.timeout:
